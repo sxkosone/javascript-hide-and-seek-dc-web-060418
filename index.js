85082680fb8a5bb307775cbda34a9e6ba089176b
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-17 18:46:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selectorsString) {
+    return document.querySelector(selectorsString);
+}
 
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    let answer;
+    function getDeeper(node) {
+        let newChild = node.querySelector("div")
+        if (newChild) {
+            getDeeper(newChild)
+        } else {
+            answer = node;
+        }
+    }
+    getDeeper(document.querySelector('#grand-node'));
+    return answer;
+    
+
+}
+
+function increaseRankBy(n) {
+    const items = document.querySelectorAll(".ranked-list li")
+    for (let li of items) {
+        li.innerHTML = parseInt(li.innerHTML) + n;
+    }
+}
